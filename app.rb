da09ab327f81cba037c6fd348ecfe49c7faffa59
Maxcell Wilson <maxcell.wilson@gmail.com>
2017-07-20 14:50:37 -0400

    Add functionality to gather tweets from two places

--- app.rb
+++ app.rb
@@ -1,18 +1,31 @@
+require 'redis'
+
 require_relative './lib/twitter_scraper'
 
-techspeakdigest = TwitterScraper.new('techspeakdigest')
+redis = Redis.new
 
-techspeadigest_cfps = techspeakdigest.tweets.map do |tweet|
+techspeakdigest = TwitterScraper.new('techspeakdigest', redis.get('techspeakdigest_latest'))
+
+techspeakdigest_tweets = techspeakdigest.tweets
+
+techspeakdigest_cfps = techspeakdigest_tweets.map do |tweet|
   if tweet.uris.first
     url = tweet.uris.first.expanded_url.to_s
     url if /tinyletter/.match(url)
   end
 end.compact
 
-mozillatech = TwitterScraper.new('mozTechCFPs')
+redis.set('techspeakdigest_latest', techspeakdigest_tweets.first.id) unless techspeakdigest_tweets.empty?
 
-mozillatech_cfps = mozillatech.tweets.map do |tweet|
+mozillatech = TwitterScraper.new('mozTechCFPs', redis.get('mozillatech_latest'))
+
+mozillatech_tweets = mozillatech.tweets
+
+mozillatech_cfps = mozillatech_tweets.map do |tweet|
   tweet.text if /cfp/i.match(tweet.text)
 end.compact
 
-p mozillatech_cfps
+redis.set('mozillatech_latest', mozillatech_tweets.first.id) unless mozillatech_tweets.empty?
+
+cfps = techspeakdigest_cfps + mozillatech_cfps
+p cfps
