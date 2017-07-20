08bdb52f4cafe94794d0e9925c9cdbcf2b1c2976
Maxcell Wilson <maxcell.wilson@gmail.com>
2017-07-20 14:14:40 -0400

    Add logic for twitter accounts

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
-require 'sinatra'
+require_relative './lib/twitter_scraper'
 
-get '/' do
-  'Put this in your pipe & smoke it!'
-end
+techspeakdigest = TwitterScraper.new('techspeakdigest')
+
+techspeadigest_cfps = techspeakdigest.tweets.map do |tweet|
+  if tweet.uris.first
+    url = tweet.uris.first.expanded_url.to_s
+    url if /tinyletter/.match(url)
+  end
+end.compact
+
+mozillatech = TwitterScraper.new('mozTechCFPs')
+
+mozillatech_cfps = mozillatech.tweets.map do |tweet|
+  tweet.text if /cfp/i.match(tweet.text)
+end.compact
+
+p mozillatech_cfps
